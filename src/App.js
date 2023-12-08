6906bbf16e328953723f6387b913a9bc385668d5
Harut20024 <htarzyanh@gmail.com>
2023-12-08 19:36:36 +0400

    adding ladscope mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Mainer from './Componets/Main/Main';
 
 function App() {
   const [showMain, setShowMain] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,13 +20,26 @@ function App() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="Canvas">
-      <main>
-        <SCanvas />
-      </main>
-      <Mainer isVisible={showMain} />
-    </div>
+    windowWidth > 640 ?
+      <div className="Canvas">
+        <main>
+          <SCanvas />
+        </main>
+        <Mainer isVisible={showMain} />
+      </div> : 
+      <div style={{ textAlign: 'center', margin: 'auto', padding: '20px',color: "#65e7e0" }}>
+        Please rotate your device to landscape mode to use this site.
+      </div>
   );
 }
 
